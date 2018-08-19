1d40f008260fbaed5dd68944c3644a4edb7bb006
Terry Yanko <terryyanko@gmail.com>
2018-08-19 21:53:46 +0000

    add some new stuff

---
+++ collect.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#dir paths
+fonts="/home/ulysses/.local/share/fonts"
+i3="/home/ulysses/.config/i3"
+ohMyZsh="/home/ulysses/.oh-my-zsh/custom"
+dirPaths=($ohMyZsh $fonts $i3) 
+
+#file paths
+zsh="/home/ulysses/.zshrc"
+xdefaults="/home/ulysses/.Xdefaults"
+filePaths=($zsh $xdefaults)
+
+#collect dirs
+for dir in ${dirPaths[@]}; do cp -R ${dir} .; done 	
+
+#collect files
+for file in ${filePaths[@]}; do cp ${file} .; done 	
